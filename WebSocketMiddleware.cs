b4c572df94824cc815a7a13c9cce8d041eacbb0c
Zehua-Chen <peterchen06242000@outlook.com>
2020-09-23 14:02:13 -0500

    [feat] Send Data

--- WebSocketMiddleware.cs
+++ WebSocketMiddleware.cs
@@ -1,9 +1,14 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Net.WebSockets;
+using System.Linq;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 
 namespace MM26TestServer
 {
@@ -21,8 +26,9 @@ namespace MM26TestServer
                 if (context.WebSockets.IsWebSocketRequest)
                 {
                     WebSocket ws = await context.WebSockets.AcceptWebSocketAsync();
-                    var handler = ActivatorUtilities.CreateInstance<WebSocketConnectionHandler>(context.RequestServices);
-                    await handler.Handle(ws);
+                    var configuration = ActivatorUtilities.GetServiceOrCreateInstance<IConfiguration>(context.RequestServices);
+
+                    await Handle(ws, configuration);
                 }
                 else
                 {
@@ -32,6 +38,26 @@ namespace MM26TestServer
             else
             {
                 await next();
+            }
+        }
+
+        private static async Task Handle(WebSocket ws, IConfiguration configuration)
+        {
+            var protoConfig = configuration.GetSection(ProtoConfiguration.Proto)
+                .Get<ProtoConfiguration>();
+
+            IEnumerable<string> files = Directory.EnumerateFiles(protoConfig.Source)
+                .OrderBy(s => s)
+                .Where(path => Path.GetExtension(path) == ".pb");
+
+            foreach (var file in files)
+            {
+                byte[] content = await File.ReadAllBytesAsync(file);
+                await ws.SendAsync(
+                    new ArraySegment<byte>(content),
+                    WebSocketMessageType.Binary,
+                    true,
+                    CancellationToken.None);
             }
         }
     }
